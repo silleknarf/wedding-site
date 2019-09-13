e23a683e4ee25cf949c1e4143f1179e09a2e8ef8
silleknarf@gmail.com <silleknarf@gmail.com>
2019-09-13 23:10:40 +0100

    Added countdown timer and moved js into separate files

---
+++ js/countdown.js
@@ -0,0 +1,36 @@
+/* Countdown timer script, nicked and modified from: 
+ * https://www.w3schools.com/howto/howto_js_countdown.asp
+ */
+
+// Our ceremony kick off time
+var countDownDate = new Date("2019-12-14 23:00:00Z").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get today's date and time
+var now = new Date().getTime();
+
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Display the result in the element with id="demo"
+var daysSection = days !== 0 ? days + " days, " : "";
+var hoursSection = hours !== 0 ? hours + " hours, " : "";
+var minutesSection = minutes !== 0 ? minutes + " minutes" : "";
+var secondsSection = seconds !== 0 ? " and " + seconds + " seconds" : "";
+
+document.getElementById("timer").innerHTML = daysSection + hoursSection + minutesSection + secondsSection;
+
+// If the count down is finished, write some text 
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Just Married";
+}
+}, 1000);
---
+++ js/tabs.js
@@ -0,0 +1,19 @@
+/* Simple script for tabs, nicked and modified from: 
+ * https://www.w3schools.com/howto/howto_js_tabs.asp
+ */
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
